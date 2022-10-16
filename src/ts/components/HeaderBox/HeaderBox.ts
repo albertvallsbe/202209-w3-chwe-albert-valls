1f4993ef6f6fe907057d39408e000c52818c73bc
Albert Valls <avalls89@gmail.com>
2022-10-16 11:36:07 +0200

    Edit header

---
+++ src/ts/components/HeaderBox/HeaderBox.ts
@@ -0,0 +1,13 @@
+import Component from "../Component/Component.js";
+
+class HeaderBox extends Component {
+  constructor(parentElement: HTMLElement) {
+    super(parentElement, "main-header3", "header");
+  }
+
+  render() {
+    super.render();
+  }
+}
+
+export default HeaderBox;
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,14 @@
 import Component from "./components/Component/Component.js";
 import Header from "./components/Header/Header.js";
+import HeaderBox from "./components/HeaderBox/HeaderBox.js";
 
 const root = document.querySelector(".root");
 
 const header = new Header(root as HTMLElement);
 header.render();
 
-const header2 = new Component(root as HTMLElement, "main-header", "header");
+const header2 = new Component(root as HTMLElement, "main-header2", "header");
 header2.render();
+
+const headerBox = new HeaderBox(root as HTMLElement);
+headerBox.render();
